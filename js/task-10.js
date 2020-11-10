abad512133754dd9077a46be60564c7674770c62
Olena Tykhonovych <olena.tykhonovych@gmail.com>
2020-11-10 21:44:38 +0200

    10 задача готова

--- js/task-10.js
+++ js/task-10.js
@@ -1,9 +1,5 @@
-// reduce
-// Получи общую сумму баланса (сумму значений свойства balance) всех пользователей.
-
-// Используй деструктурирующее присваивание для параметра функции {balance} без пробелов и переносов на новую строку
-
-// Используй только перебирающие методы массива которые не изменяют (не мутируют) исходный массив. Т.е. нельзя использовать for, splice, push и т.п. мутирующие методы.
+// reduce, filter, sort
+// Получи массив всех умений всех пользователей (поле skills), при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
 
 const users =  [
   {
@@ -93,8 +89,20 @@ const users =  [
 ];
 
 // Write code under this line
-const calculateTotalBalance = array =>
-  array.reduce((array, { balance }) => array + balance, 0);
+const getSortedUniqueSkills = array =>
+  array
+    .reduce((acc, item) => [...acc, ...item.skills], [])
+    .filter((current, index, arr) => arr.indexOf(current) === index)
+    .sort();
 
-// console.log(calculateTotalBalance(users));
-// 20916
+// console.log(getSortedUniqueSkills(users));
+/* [ 'adipisicing', 'amet',
+ 'anim', 'commodo',
+ 'culpa', 'elit',
+ 'ex', 'ipsum',
+ 'irure', 'laborum',
+ 'lorem', 'mollit',
+ 'non', 'nostrud',
+ 'nulla', 'proident',
+ 'tempor', 'velit',
+ 'veniam' ]; */
